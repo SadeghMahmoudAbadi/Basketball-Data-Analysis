0bdd6c17fec3f4aa57de51e639987fc413f7ca61
Sadegh Mahmoud Abadi <59364741+SadeghMahmoudAbadi@users.noreply.github.com>
2026-07-18 18:47:42 +0330

    Update create_database.py
    Changed varchar(255) to text (apparently in sqlite3, text is used either way)

--- create_database.py
+++ create_database.py
@@ -13,42 +13,42 @@ cursor.execute("PRAGMA foreign_keys = ON;")
 # Teams
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS teams (
-    team_id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
-    city VARCHAR(255),
-    state VARCHAR(255)
+    team_id TEXT PRIMARY KEY,
+    name TEXT,
+    city TEXT,
+    state TEXT
 );
 """)
 
 # Players
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS players (
-    player_id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
+    player_id TEXT PRIMARY KEY,
+    name TEXT,
     birthdate DATE,
     height INTEGER,
     weight INTEGER,
-    shoots VARCHAR(255),
-    nationality VARCHAR(255),
-    college VARCHAR(255)
+    shoots TEXT,
+    nationality TEXT,
+    college TEXT
 );
 """)
 
 # Coaches
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS coaches (
-    coach_id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
+    coach_id TEXT PRIMARY KEY,
+    name TEXT,
     birthdate DATE,
-    nationality VARCHAR(255)
+    nationality TEXT
 );
 """)
 
 # Awards
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS awards (
-    award_id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255)
+    award_id TEXT PRIMARY KEY,
+    name TEXT
 );
 """)
 
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS awards (
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS seasons (
     season_id INTEGER PRIMARY KEY,
-    champion_id VARCHAR(255),
+    champion_id TEXT,
     FOREIGN KEY (champion_id) REFERENCES teams(team_id)
 );
 """)
@@ -65,8 +65,8 @@ CREATE TABLE IF NOT EXISTS seasons (
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS player_position (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    player_id VARCHAR(255) NOT NULL,
-    position VARCHAR(255) NOT NULL,
+    player_id TEXT NOT NULL,
+    position TEXT NOT NULL,
     FOREIGN KEY (player_id) REFERENCES players(player_id)
 );
 """)
@@ -76,8 +76,8 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS award_season (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     season_id INTEGER NOT NULL,
-    award_id VARCHAR(255) NOT NULL,
-    player_id VARCHAR(255) NOT NULL,
+    award_id TEXT NOT NULL,
+    player_id TEXT NOT NULL,
     FOREIGN KEY (season_id) REFERENCES seasons(season_id),
     FOREIGN KEY (award_id) REFERENCES awards(award_id),
     FOREIGN KEY (player_id) REFERENCES players(player_id)
@@ -89,46 +89,36 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS player_stats (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     season_id INTEGER NOT NULL,
-    player_id VARCHAR(255) NOT NULL,
-    team_id VARCHAR(255) NOT NULL,
-
+    player_id TEXT NOT NULL,
+    team_id TEXT NOT NULL,
     games INTEGER,
     games_started INTEGER,
     minutes_played INTEGER,
-
     field_goals INTEGER,
     field_goal_attempts INTEGER,
     field_goal_percent REAL,
-
     three_point_field_goals INTEGER,
     three_point_field_goal_attempts INTEGER,
     three_point_field_goal_percent REAL,
-
     two_point_field_goals INTEGER,
     two_point_field_goal_attempts INTEGER,
     two_point_field_goal_percent REAL,
-
     effective_field_goal_percent REAL,
-
     free_throws INTEGER,
     free_throw_attempts INTEGER,
     free_throw_percent REAL,
-
     offensive_rebounds INTEGER,
     defensive_rebounds INTEGER,
     total_rebounds INTEGER,
-
     assists INTEGER,
     steals INTEGER,
     blocks INTEGER,
     turnovers INTEGER,
     personal_fouls INTEGER,
-
     points INTEGER,
     triple_doubles INTEGER,
     win_shares REAL,
     experience INTEGER,
-
     FOREIGN KEY (season_id) REFERENCES seasons(season_id),
     FOREIGN KEY (player_id) REFERENCES players(player_id),
     FOREIGN KEY (team_id) REFERENCES teams(team_id)
@@ -140,19 +130,16 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS team_stats (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     season_id INTEGER NOT NULL,
-    team_id VARCHAR(255) NOT NULL,
-
+    team_id TEXT NOT NULL,
     wins INTEGER,
     losses INTEGER,
     win_loss_percent REAL,
     finish_rank INTEGER,
-    playoff_result VARCHAR(255),
-
+    playoff_result TEXT,
     offensive_rating REAL,
     defensive_rating REAL,
     net_rating REAL,
     pace REAL,
-
     FOREIGN KEY (season_id) REFERENCES seasons(season_id),
     FOREIGN KEY (team_id) REFERENCES teams(team_id)
 );
@@ -163,12 +150,10 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS coach_stats (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     season_id INTEGER NOT NULL,
-    coach_id VARCHAR(255) NOT NULL,
-    team_id VARCHAR(255) NOT NULL,
-
+    coach_id TEXT NOT NULL,
+    team_id TEXT NOT NULL,
     wins_under_coach INTEGER,
     losses_under_coach INTEGER,
-
     FOREIGN KEY (season_id) REFERENCES seasons(season_id),
     FOREIGN KEY (coach_id) REFERENCES coaches(coach_id),
     FOREIGN KEY (team_id) REFERENCES teams(team_id)
