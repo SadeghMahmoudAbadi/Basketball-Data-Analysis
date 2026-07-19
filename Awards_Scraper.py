79e5d2a2053f1e4affa415ee09105d6d0636aa40
Sadegh Mahmoud Abadi <59364741+SadeghMahmoudAbadi@users.noreply.github.com>
2026-07-19 21:38:16 +0330

    Connect Awards_Scraper.py to database
    Connected awards and award season to database
    Inserted awards and award season into database

--- Awards_Scraper.py
+++ Awards_Scraper.py
@@ -1,6 +1,7 @@
 import asyncio
-import re
 from playwright.async_api import async_playwright
+from models import Award, AwardSeason
+import sqlite3
 
 BASE_URL = "https://www.basketball-reference.com"
 
@@ -16,7 +17,9 @@ AWARDS_MAP = {
     "mip": {"name": "George Mikan Trophy", "table_id": "table#mip"}
 }
 
-def log(message):
+DATABASE = "basketball_reference.db"
+
+def log(message) -> None:
     print(f"[Awards Scraper] {message}", flush=True)
 
 async def safe_goto(page, url: str, retries: int = 4, delay: int = 7) -> bool:
@@ -29,16 +32,18 @@ async def safe_goto(page, url: str, retries: int = 4, delay: int = 7) -> bool:
             await asyncio.sleep(delay)
     return False
 
-async def scrape_nba_awards_data(start_year: int, end_year: int):
+async def scrape_nba_awards_data(start_year: int, end_year: int) -> tuple[list[Award], list[AwardSeason]]:
     log("در حال راه‌اندازی مرورگر برای بخش جوایز...")
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=True, channel='chrome')
         context = await browser.new_context(user_agent=HEADERS["User-Agent"])
         page = await context.new_page()
-        
-        awards_list = [{"award_id": a_id, "name": info["name"]} for a_id, info in AWARDS_MAP.items()]
+        # Store awards in awards_list
+        awards_list = []
+        for award_id, info in AWARDS_MAP.items():
+            awards_list.append(Award(award_id, info['name']))
+        # Store season awards in award_seasons_list
         award_seasons_list = []
-
         for year in range(start_year, end_year + 1):
             awards_url = f"{BASE_URL}/awards/awards_{year}.html"
             log(f"در حال استخراج جوایز فصل {year}...")
@@ -53,23 +58,36 @@ async def scrape_nba_awards_data(start_year: int, end_year: int):
                         if await player_link.count() > 0:
                             href = await player_link.get_attribute("href")
                             p_id = href.split("/")[-1].replace(".html", "")
-                            
-                            award_seasons_list.append({
-                                "season_id": year,
-                                "award_id": a_id,
-                                "player_id": p_id
-                            })
+                            award_seasons_list.append(AwardSeason(year, a_id, p_id))
                 
                 await asyncio.sleep(2)
             else:
                 log(f"خطای جدی: موفق به باز کردن صفحه جوایز سال {year} نشدیم.")
         
         await browser.close()
-        return awards_list, award_seasons_list
+        return (awards_list, award_seasons_list)
+
+def insert_data(awards: list[Award], award_season_list: list[AwardSeason]) -> None:
+    """Insert coaches and coach_stats into database"""
+    try:
+        # Connect to SQLite database
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        # Insert coaches into database
+        for award in awards:
+            award.insert_award(cursor)
+        # Insert coach_stats into database
+        for award_season in award_season_list:
+            award_season.insert_award_season(cursor)
+        # Commit changes and close connection
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        log(f"خطا در وارد کردن دیتا به دیتابیس: {e}")
 
 async def main():
     awards, award_seasons = await scrape_nba_awards_data(2019, 2024)
-    
+    insert_data(awards, award_seasons)
     log(f"\n==================== پایان عملیات جوایز ====================")
     log(f"تعداد کل انواع جوایز تعریف شده: {len(awards)}")
     log(f"تعداد کل رکوردهای ثبت شده (رابطه بازیکن-جایزه-سال): {len(award_seasons)}")
